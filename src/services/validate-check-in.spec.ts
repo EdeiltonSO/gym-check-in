4fdf32f04b1b09343efa32fad3bc7b9d673af133
Edeilton Oliveira <edeiltonso@gmail.com>
2024-05-12 20:54:53 -0300

    feat: limita validação do check-in em até 20 minutos após a criação

--- src/services/validate-check-in.spec.ts
+++ src/services/validate-check-in.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { beforeEach, afterEach, describe, expect, it } from 'vitest'
+import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest'
 import { ValidateCheckInService } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
@@ -11,11 +11,11 @@ describe('Check-in Service', () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInService(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check-in', async () => {
@@ -37,5 +37,24 @@ describe('Check-in Service', () => {
         checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2024, 0, 1, 0, 0))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const TWENTY_ONE_MINUTES_IN_MS = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(TWENTY_ONE_MINUTES_IN_MS)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
