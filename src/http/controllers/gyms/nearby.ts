f4de3db64e9ab3432e46d9f744298ffbbeb715ce
Edeilton Oliveira <edeiltonso@gmail.com>
2024-05-19 19:31:45 -0300

    feat: cria rotas de busca de academias

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeFetchNearbyGymsService } from '@/services/factories/make-fetch-nearby-gym-service'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  const searchGymsService = makeFetchNearbyGymsService()
+
+  const { gyms } = await searchGymsService.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
